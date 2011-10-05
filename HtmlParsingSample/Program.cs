56462c59125770a225c0c6d1d23aaf0819cd83fc
Andy Sherwood <xinmyname@gmail.com>
2011-10-05 13:30:41 -0700

    Added (sorta) HTML parser sample

---
+++ HtmlParsingSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using Sgml;
+using xizzle;
+
+namespace HtmlParsingSample
+{
+    /// <summary>
+    /// Use SgmlReader and xizzle to parse HTML, like sharp-query does
+    /// Big caveat - there's no way to select a class. I'm thinking of adding a convention to handle that,
+    /// but I just need XML parsing right now.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testPageStream = typeof(Program).Assembly.GetManifestResourceStream("HtmlParsingSample.TestPage.htm");
+
+            using (var reader = SgmlReader.Create(testPageStream))
+            using (var context = XElementContext.Load(reader))
+            {
+                Console.WriteLine(context.Select("#first").Single().Value);
+
+                var itemOptions = context.Select("#items option");
+
+                Console.WriteLine("There are {0} items.", itemOptions.Count());
+
+                foreach (XElement optionElement in itemOptions)
+                    Console.WriteLine("    {0}) {1}", optionElement.Attribute("value").Value, optionElement.Value);
+            }
+        }
+    }
+}
